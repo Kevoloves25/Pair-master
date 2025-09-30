b1b806d3a7b2ddc8c20348fc9af0550a20552bf3
KevoLoves25 <kakemu84@gmail.com>
2025-10-01 00:01:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Telegraf, Markup } = require('telegraf');
 const axios = require('axios');
 
 // ==================== CONFIGURATION ====================
-const BOT_TOKEN = '7701970165:AAHf-r1xxxxxxxxxxxxxxxxxxxxxxxxxxx'; // Your token
+const BOT_TOKEN = '7701970165:AAFmPpYOJ92MT033UoJLmxfQX7rIe703k6E'; // Your token
 
 // BOTS WITH THEIR ACTUAL PAIRING SITES AND FORM DETAILS
 const BOTS_DATA = [
